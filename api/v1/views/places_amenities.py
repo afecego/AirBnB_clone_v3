24504adf6566af40f5c150a61f9ec664a7eba882
JuanSebastianGB <3393@holbertonschool.com>
2022-02-14 03:13:18 +0000

    FIX: Fixing and testing post request

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -26,7 +26,7 @@ def get_place_amenities(place_id):
     if environ.get('HBNB_TYPE_STORAGE') == "db":
         amenities = [amenity.to_dict() for amenity in place.amenities]
     else:
-        amenities = [storage.get(Amenity, amenity_id).to_dict()
+        amenities = [storage.get('Amenity', amenity_id).to_dict()
                      for amenity_id in place.amenity_ids]
 
     return jsonify(amenities)
@@ -63,19 +63,18 @@ def post_place_amenity(place_id=None, amenity_id=None):
     Link a Amenity object to a Place
     """
     place = storage.get('Place', place_id)
-
     if place is None:
         abort(404, 'Not found')
-
     amenity = storage.get('Amenity', amenity_id)
-
     if amenity is None:
         abort(404, 'Not found')
-
     if amenity in place.amenities or amenity.id in place.amenities:
         return jsonify(amenity.to_dict()), 200
     if environ.get('HBNB_TYPE_STORAGE') == "db":
-        place.amenities.append(amenity)
+        place_amenities = place.amenities
     else:
-        place.amenities = amenity
-    return jsonify(amenity.to_dict()), 201
+        place_amenities = place.amenity_ids
+        return jsonify(amenity.to_dict())
+    place_amenities.append(amenity)
+    place.save()
+    return make_response(jsonify(amenity.to_dict()), 201)
